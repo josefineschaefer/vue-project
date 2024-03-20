68924c29f1f39e9688441afa7138291f491afa94
Josefine Schaefer <schaeferj@hotmail.de>
2024-03-20 18:04:31 +0100

    add tab index

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+      tabindex="3"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
